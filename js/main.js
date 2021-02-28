1f53322dfd29ac8935c5cd152c1421085610899e
antixrizt <79777980+antixrizt@users.noreply.github.com>
2021-02-28 06:06:10 +0200

    new site

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+
+
+ $('.sliderpage__image').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.sliderpage__text-inner',
+  arrows:false,
+  verticalSwiping:false,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        
+      }
+    },
+  ]
+ 
+});
+$('.sliderpage__text-inner').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.sliderpage__image',
+  fade: true,
+  prevArrow: false,
+  dots:true,
+  nextArrow:'<button  class="slick-btn slick-next"><img src="../img/arrow-r.svg" alt="next"></button>',
+  responsive: [
+    {
+      breakpoint: 732,
+      settings: {
+        arrows: false,
+        dots:false
+      }
+    },
+  ]
+  });
+
+  $('.header__link-menu').click(function(event){
+  $('.header__link-menu,.header__menu-inner').toggleClass('active')
+  });
+ 
+
+});
